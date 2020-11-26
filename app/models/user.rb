58b94b1c744e21c37103de4004efff11e2cdaa23
Michał Zając <michal.zajac@gmail.com>
2020-11-26 18:25:10 +0100

    Add registered user count to homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_secure_password
   has_many :votes, dependent: :destroy
+
+  scope :registered_users, -> { where.not(confirmed_at: nil).count }
 end
